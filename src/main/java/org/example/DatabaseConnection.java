a03251f6cd7c0a1e4e89e46d9bde46a6fa3ca9e4
DamianLopatowski <lopatowskidamian@gmail.com>
2025-01-09 00:38:17 +0100

    aggiunta

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -3,16 +3,20 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DatabaseConnection {
     public static Connection connectToDatabase() {
-        try {
-            // Connessione al database
-            String url = "jdbc:mysql://localhost:3306/bikegarage";
-            String username = "root";
-            String password = "Kazik+10";
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
+            String url = properties.getProperty("db.url");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
             return DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             System.out.println("Errore di connessione: " + e.getMessage());
             return null;
         }
