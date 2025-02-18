95820c6c3d8c366943bb6126025f9203dd613329
DamianLopatowski <lopatowskidamian@gmail.com>
2025-02-18 22:40:39 +0100

    aggiunta

--- src/main/java/org/example/controllerapplicativo/NavigationController.java
+++ src/main/java/org/example/controllerapplicativo/NavigationController.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.controllergrafici.GestioneController;
 import org.example.dao.ClienteDAO;
